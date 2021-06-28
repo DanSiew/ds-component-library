4da0bdc3678243bb7ee4a2d50929d37a41bcd585
Danny <dktsiew@hotmail.com>
2021-06-28 17:03:42 +1000

    Update button component

--- src/components/atoms/button/button.component.tsx
+++ src/components/atoms/button/button.component.tsx
@@ -1,29 +1,22 @@
 import React from "react";
 import "./button.style.scss";
-import { ButtonProps, ButtonType } from "./button.types";
+import { ButtonProps } from "./button.types";
 
 class DsButtonComponent extends React.Component<ButtonProps> {
-  private divProps: any;
-
-  constructor(props:ButtonProps){
+  constructor(props: ButtonProps) {
     super(props);
-    this.divProps = Object.assign({}, this.props);
-    delete this.divProps.buttonType;
-    delete this.divProps.backgroundColor;
   }
 
   render() {
     return (
       <button
         aria-label={this.props.label}
-        type="button"
+        type={this.props.type}
         className={[
           "ds-button",
           `ds-button--${this.props.size}`,
           `ds-button--${this.props.buttonType}`,
         ].join(" ")}
-        style={{ backgroundColor: this.props.backgroundColor }}
-        {...this.divProps}
         onClick={this.props.onClick}
       >
         {this.props.label}
--- src/components/atoms/button/button.test.tsx
+++ src/components/atoms/button/button.test.tsx
@@ -1,38 +1,54 @@
 import * as React from "react";
+import renderer from "react-test-renderer";
 import { render, fireEvent, screen } from "@testing-library/react";
 import DsButtonComponent from "./button.component";
 
 describe("Button", () => {
-  test("renders a secondary button with text", async () => {
-    render(
+  test("creates a snapshot of the button", async () => {
+    const component = renderer.create(
       <DsButtonComponent
+        type="submit"
         size="small"
-        buttonType="secondary"
+        buttonType="primary"
         label="Click me"
       ></DsButtonComponent>
     );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
+  test("renders a primary button with text", async () => {
+    const { container } = render(
+      <DsButtonComponent
+        type="submit"
+        size="small"
+        buttonType="primary"
+        label="Click me"
+      ></DsButtonComponent>
+    );
+    expect(container.firstChild).toHaveClass("ds-button--primary");
+    expect(container.getElementsByClassName("ds-button--primary")).toBeTruthy();
     expect(screen.getByText("Click me")).toBeInTheDocument();
   });
 
-  test("renders a button with custom colors", async () => {
-    render(
+  test("renders a secondary button with text", async () => {
+    const { container } = render(
       <DsButtonComponent
+        type="button"
         size="small"
         buttonType="secondary"
-        backgroundColor="#A78BFA"
         label="Click me"
       ></DsButtonComponent>
     );
-
-    expect(screen.getByText("Click me")).toHaveStyle({
-      backgroundColor: "#A78BFA",
-    });
+    expect(container.firstChild).toHaveClass("ds-button--secondary");
+    expect(screen.getByText("Click me")).toBeInTheDocument();
   });
+
   test("handles onClick", async () => {
     const mockOnClick = jest.fn();
     render(
       <DsButtonComponent
+        type="button"
         size="small"
         buttonType="secondary"
         onClick={mockOnClick}
